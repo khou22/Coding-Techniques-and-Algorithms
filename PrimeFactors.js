f4378f3e0fa605003cca3247c921e7dd9e3baaa9
Kevin Hou <kevin.ch.hou@gmail.com>
2016-12-05 20:43:39 -0500

    Updated strategy

--- PrimeFactors.js
+++ PrimeFactors.js
@@ -3,11 +3,12 @@ http://emea.emc.com/emc-plus/rsa-labs/historical/the-rsa-challenge-numbers.htm
 
 Hints:
 * The two numbers will be roughly the same size
-* A 300 bit number will yield two roughly 150 (+/- 1) bit prime factors
+* A 50 bit number will yield two roughly 25 (± 1) bit prime factors
 
 Strategy:
 * Cycle through set of primes within that region
-* Mulitply ones digit of two numbers and compare to product's ones digit
+* Multitly ones digit of two numbers and compare to product's ones digit
 * Speed up the process of checking all the primes
-
+* It's very inefficient to calculate large numbers primes
+* Factoring is also intractible making it a very useful element of cryptography
 */
