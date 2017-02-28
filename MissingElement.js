73238ea4303594a092d6e1d1e06495ef003d2d7f
Kevin Hou <kevin.ch.hou@gmail.com>
2017-02-28 14:56:53 -0500

    New algorithm on finding missing elements from a stream
    COS 226 class meeting on interview questions (2/27/17)

---
+++ MissingElement.js
@@ -0,0 +1,24 @@
+// Setup stream array with only one missing element
+var N = 4;
+var missing = 3;
+var stream = [];
+for (var i = 0; i < N; i++) {
+  if (i + 1 != missing) { // If not the missing element
+    stream.push(i + 1); // Add element
+  }
+}
+// Print problem
+console.log(stream);
+console.log("What is the missing number between 1 and " + N + "?");
+console.log(); // Line break
+
+// Solution
+var sum = 0;
+for (var i = 0; i < stream.length; i++) {
+  sum += stream[i]; // Sum
+}
+var actualSum = (N * (N + 1)) / 2; // Sum of all numbers between 1 and N
+var missingNumber = actualSum - sum;
+
+console.log("Missing number: " + missingNumber);
+console.log("Performance: linear time");
